4e828f0bbfa50984748fef94e8db7759e891a526
Doug Wollison <doug@premise.ca>
2016-02-01 13:47:23 -0500

    Ditched Loader class; moved uninstall hook to System.

--- inc/class-indexpages-system.php
+++ inc/class-indexpages-system.php
@@ -35,8 +35,8 @@ class System {
 	 * @since 1.0.0
 	 */
 	public static function setup() {
-		// Register the loader hooks
-		Loader::register_hooks();
+		// Register the uninstall hook
+		register_uninstall_hook( INDEXPAGES_PLUGIN_FILE, array( static::$name, 'uninstall' ) );
 
 		// Setup the registry
 		Registry::load();
@@ -44,6 +44,39 @@ class System {
 		// Register the hooks of the subsystems
 		Frontend::register_hooks();
 		Backend::register_hooks();
+	}
+
+	// =========================
+	// ! Uninstallation
+	// =========================
+
+	/**
+	 * Delete database tables and any options.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @global wpdb $wpdb The database abstraction class instance.
+	 */
+	public static function uninstall() {
+		global $wpdb;
+
+		// Make sure they have permisson
+		if ( ! current_user_can( 'activate_plugins' ) ) {
+			return;
+		}
+
+		if ( $check_referer ) {
+			$plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
+			check_admin_referer( "{$check_referer}-plugin_{$plugin}" );
+		} else {
+			// Check if this is the intended file for uninstalling
+			if ( __FILE__ != WP_UNINSTALL_PLUGIN ) {
+				return;
+			}
+		}
+
+		// And delete the options
+		$wpdb->query( "DELETE FORM $wpdb->options WHERE option_name LIKE 'page\_for\_%\_posts'" );
 	}
 }
 
