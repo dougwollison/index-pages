abe311c147da7abecdcfded87c3c573d25abaf87
Doug Wollison <doug@wollison.net>
2016-02-02 22:43:48 -0500

    Removed uninstall hook; shouldn't bother.

--- inc/class-indexpages-system.php
+++ inc/class-indexpages-system.php
@@ -34,48 +34,12 @@ class System {
 	 * @since 1.0.0
 	 */
 	public static function setup() {
-		// Register the uninstall hook
-		register_uninstall_hook( INDEXPAGES_PLUGIN_FILE, array( get_called_class(), 'uninstall' ) );
-
 		// Setup the registry
 		Registry::load();
 
 		// Register the hooks of the subsystems
 		Frontend::register_hooks();
 		Backend::register_hooks();
-	}
-
-	// =========================
-	// ! Uninstallation
-	// =========================
-
-	/**
-	 * Delete database tables and any options.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @global wpdb $wpdb The database abstraction class instance.
-	 */
-	public static function uninstall() {
-		global $wpdb;
-
-		// Make sure they have permisson
-		if ( ! current_user_can( 'activate_plugins' ) ) {
-			return;
-		}
-
-		if ( $check_referer ) {
-			$plugin = isset( $_REQUEST['plugin'] ) ? $_REQUEST['plugin'] : '';
-			check_admin_referer( "{$check_referer}-plugin_{$plugin}" );
-		} else {
-			// Check if this is the intended file for uninstalling
-			if ( __FILE__ != WP_UNINSTALL_PLUGIN ) {
-				return;
-			}
-		}
-
-		// And delete the options
-		$wpdb->query( "DELETE FORM $wpdb->options WHERE option_name LIKE 'page\_for\_%\_posts'" );
 	}
 }
 
