ba3ea1637a9e1abe43d1162dc7a09215a0737c20
Doug Wollison <doug@wollison.net>
2017-07-28 13:47:57 -0400

    Added rewrite support for term pages

--- includes/class-indexpages-frontend.php
+++ includes/class-indexpages-frontend.php
@@ -59,6 +59,7 @@ final class Frontend extends Handler {
 		// Title/link rewriting
 		self::add_filter( 'wp_title_parts', 'rewrite_title_parts', 10, 1 );
 		self::add_filter( 'post_type_archive_link', 'rewrite_archive_link', 10, 2 );
+		self::add_filter( 'term_link', 'rewrite_term_link', 10, 2 );
 
 		// Nav menu rewriting
 		self::add_filter( 'wp_nav_menu_objects', 'handle_index_page_links', 10, 1 );
@@ -76,6 +77,7 @@ final class Frontend extends Handler {
 	 *
 	 * Also checks for date and pagination parameters.
 	 *
+	 * @since 1.4.0 Added support for term pages.
 	 * @since 1.2.0 Added check to make sure post type currently exists.
 	 * @since 1.0.0
 	 *
@@ -120,6 +122,34 @@ final class Frontend extends Handler {
 				unset( $qv['pagename'] );
 				unset( $qv['page'] );
 				unset( $qv['name'] );
+			} else
+			// Alternatively, get the term, and validate that it exists
+			if ( $term = Registry::is_term_page( $page->ID ) ) {
+				// Modify the request into a post type archive instead
+				switch ( $term->taxonomy ) {
+					case 'category':
+						$qv['cat'] = $term->term_id;
+						break;
+
+					case 'post_tag':
+						$qv['tag_id'] = $term->term_id;
+						break;
+
+					default:
+						$qv['tax_query'] = array(
+							array(
+								'taxonomy' => $term->taxonomy,
+								'field' => 'term_id',
+								'terms' => $term->term_id,
+							),
+						);
+				}
+				list( , , $qv['year'], $qv['monthnum'], $qv['day'], $qv['paged'] ) = array_pad( $matches, 6, null );
+
+				// Make sure these are unset
+				unset( $qv['pagename'] );
+				unset( $qv['page'] );
+				unset( $qv['name'] );
 			}
 		}
 	}
@@ -131,6 +161,7 @@ final class Frontend extends Handler {
 	/**
 	 * Filter the title parts to use the assigned index page's title.
 	 *
+	 * @since 1.4.0 Added support for term pages.
 	 * @since 1.0.0
 	 *
 	 * @param array $title The title parts to filter.
@@ -138,17 +169,25 @@ final class Frontend extends Handler {
 	 * @return array The filtered title parts.
 	 */
 	public static function rewrite_title_parts( array $title_parts ) {
-		// Skip if not an archive
-		if ( ! is_post_type_archive() ) {
-			return $title_parts;
-		}
+		// Handle post type index if applicable
+		if ( is_post_type_archive() ) {
+			// Get the queried post type
+			$post_type = get_query_var( 'post_type', 'post' );
 
-		// Get the queried post type
-		$post_type = get_query_var( 'post_type', 'post' );
+			// Get the index for this post type, update the title if found
+			if ( $index_page = Registry::get_index_page( $post_type ) ) {
+				$title_parts[0] = get_the_title( $index_page );
+			}
+		} else
+		// Alternatively, handle term index if applicable
+		if ( is_category() || is_tag() || is_tax() ) {
+			// Get the queried term
+			$term = get_queried_object();
 
-		// Get the index for this post type, update the title if found
-		if ( $index_page = Registry::get_index_page( $post_type ) ) {
-			$title_parts[0] = get_the_title( $index_page );
+			// Get the index for this post type, update the title if found
+			if ( $index_page = Registry::get_term_page( $term ) ) {
+				$title_parts[0] = get_the_title( $index_page );
+			}
 		}
 
 		return $title_parts;
@@ -172,6 +211,24 @@ final class Frontend extends Handler {
 		return $link;
 	}
 
+	/**
+	 * Filter the term link to use the assigned index page's permalink.
+	 *
+	 * @since 1.4.0
+	 *
+	 * @param string $link The permalink to filter.
+	 * @param object $term The term this is for.
+	 *
+	 * @return string The filtered term link.
+	 */
+	public static function rewrite_term_link( $link, $term ) {
+		if ( $index = Registry::get_term_page( $term ) ) {
+			$link = get_permalink( $index );
+		}
+
+		return $link;
+	}
+
 	// =========================
 	// ! Nav Menu Rewriting
 	// =========================
@@ -187,7 +244,7 @@ final class Frontend extends Handler {
 	 */
 	public static function handle_index_page_links( $menu_items ) {
 		// Get the current index page
-		$index_page = get_index_page();
+		$index_page = get_term_index_page() ?: get_index_page();
 
 		if ( $index_page ) {
 			foreach ( $menu_items as $menu_item ) {
@@ -211,13 +268,19 @@ final class Frontend extends Handler {
 	/**
 	 * Add an Edit Index Page button to the admin bar if applicable.
 	 *
+	 * @since 1.4.0 Added support for term pages.
 	 * @since 1.0.0
 	 *
 	 * @param WP_Admin_Bar $wp_admin_bar The admin bar object.
 	 */
 	public static function add_edit_button( \WP_Admin_Bar $wp_admin_bar ) {
 		// Abort if not an archive for the supported post types
-		if ( ! is_post_type_archive() ) {
+		if ( is_post_type_archive() ) {
+			$index_page = Registry::get_index_page( get_query_var( 'post_type' ) );
+		} else
+		if ( is_category() || is_tag() || is_tax() ) {
+			$index_page = Registry::get_term_page( get_queried_object() );
+		} else {
 			return;
 		}
 
@@ -230,7 +293,7 @@ final class Frontend extends Handler {
 		$post_type_object = get_post_type_object( 'page' );
 
 		// If an index is found, is editable, and has an edit link, add the edit button.
-		if ( ( $index_page = Registry::get_index_page( get_query_var( 'post_type' ) ) )
+		if ( $index_page
 		&& current_user_can( 'edit_post', $index_page )
 		&& $edit_post_link = get_edit_post_link( $index_page ) ) {
 			$wp_admin_bar->add_menu( array(
